70f0bd1e20bc5da5293d9efdcbbe27a4484aac91
James Dennes <jdennes@gmail.com>
2011-11-02 15:47:32 +1100

    Added white_label helper. Use as required. Closes #10.

--- application.rb
+++ application.rb
@@ -36,6 +36,10 @@ helpers do
     "/js/#{js}.js?" + File.mtime(File.join(settings.public_folder, "js", "#{js}.js")).to_i.to_s
   end
 
+  def white_label
+    return (APP_CANVAS_NAME =~ /csfbsub$/ ? true : false)
+  end
+
   def att_friendly_key(key)
     "cf-#{key[1..-2]}"
   end
