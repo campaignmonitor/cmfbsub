a0b8b3720760c75f5a4e5ba280e136a214319858
James Dennes <jdennes@gmail.com>
2012-02-27 16:59:03 +0430

    Added month, day, and year fields for date type fields in subscribe forms. Also added JavaScript to ensure invalid input (e.g. 31-Feb-2012) is disallowed. Fixes #13.

--- application.rb
+++ application.rb
@@ -59,6 +59,30 @@ helpers do
     end
   end
 
+  def get_months
+    [
+      {:index => 1, :name => "Jan"},
+      {:index => 2, :name => "Feb"},
+      {:index => 3, :name => "Mar"},
+      {:index => 4, :name => "Apr"},
+      {:index => 5, :name => "May"},
+      {:index => 6, :name => "Jun"},
+      {:index => 7, :name => "Jul"},
+      {:index => 8, :name => "Aug"},
+      {:index => 9, :name => "Sep"},
+      {:index => 10, :name => "Oct"},
+      {:index => 11, :name => "Nov"},
+      {:index => 12, :name => "Dec"}
+    ]
+  end
+  
+  def get_days
+    (1..31)
+  end
+  
+  def get_years
+    (1900..2048)
+  end
 end
 
 before do
--- public/js/subscribe_form.js
+++ public/js/subscribe_form.js
@@ -1,12 +1,22 @@
 (function (cmfbsub, $) {
   
   var config = {};
+  
+  function prepareDateFields() {
+    $("fieldset.date-fieldset").each(function () {
+      var $year = $(this).find('select[id^="year-"]'),
+          $month = $(this).find('select[id^="month-"]'),
+          $day = $(this).find('select[id^="day-"]');
+      $(this).find('input[id^="cf-"]').val($year.val() + "-" + $month.val() + "-" + $day.val());
+    });
+  }
 
   function setupSubscribe() {
     $("form#subscribeform").validate({errorElement: "div"});
     $("form#subscribeform").submit(function() { return false; });
     $("button#subscribe").click(function() {
       if (!$("form#subscribeform").valid()) { return; }
+      prepareDateFields();
       $("button#subscribe").addClass('disabled').html('Subscribing&hellip;');
       $.ajax({
         type: "POST",
@@ -24,7 +34,7 @@
           }
           $("button#subscribe").removeClass('disabled').html('Subscribe');
         },
-        error: function() {
+        error: function(xhr, status, error) {
           $("button#subscribe").removeClass('disabled').html('Subscribe');
           $(".subscribe-error p.info").text(data.message);
           $(".subscribe-error").show().fadeTo(0,0).animate({opacity:1}, 1500, 'easeOutExpo');
@@ -32,33 +42,48 @@
       });
     });
   }
-  
-  function addValidators() {
-    $.validator.addMethod(
-    	"subscribeformdate", function(value, element) {
-    		var check = false,
-    		    re = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
-    		if( re.test(value)){
-    			var adata = value.split('/'),
-    			    gg = parseInt(adata[0],10),
-    			    mm = parseInt(adata[1],10),
-    			    aaaa = parseInt(adata[2],10),
-    			    xdata = new Date(aaaa,mm-1,gg);
-    			if ( ( xdata.getFullYear() == aaaa ) && ( xdata.getMonth () == mm - 1 ) && ( xdata.getDate() == gg ) )
-    				check = true;
-    			else
-    				check = false;
-    		} else
-    			check = false;
-    		return this.optional(element) || check;
-    	},
-    	"Please enter a valid date (dd/mm/yyyy)"
-    );
+
+  function setupDateFieldSet($month, $day, $year) {
+    var onDateValChange = function() {
+      var year = $year.val(),
+          month = $month.val();
+      if ((year != 0) &&  (month!=0)) {
+        var lastday = 32 - new Date(year, month - 1, 32).getDate(),
+            selected_day = $day.val();
+        // Change selected day if it is greater than the number of days in current month
+        if (selected_day > lastday) {
+          $day.find('option[value=' + selected_day + ']').attr('selected', false);
+          $day.find('#dob_day  > option[value=' + lastday + ']').attr('selected', true);
+        }
+        // Remove possibly offending days
+        for (var i = lastday + 1; i < 32; i++) {
+          $day.find('option[value=' + i + ']').remove();  
+        }
+        // Add possibly missing days
+        for (var i = 29; i < lastday + 1; i++) {
+          if (!$day.find('option[value=' + i + ']').length) {
+            $day.append($("<option></option>").attr("value",i).text(i));
+          } 
+        }
+      }
+    }
+    $month.change(onDateValChange);
+    $year.change(onDateValChange);
+  }
+
+  function setupDateFields() {
+    var $fieldsets = $("fieldset.date-fieldset");
+    $fieldsets.each(function () {
+      var $month = $(this).find('select[id^="month-"]'),
+          $day = $(this).find('select[id^="day-"]'),
+          $year = $(this).find('select[id^="year-"]');
+      setupDateFieldSet($month, $day, $year);
+    });
   }
 
   function ready(data) {
     config = data;
-    addValidators();
+    setupDateFields();
     setupSubscribe();
   }
 
