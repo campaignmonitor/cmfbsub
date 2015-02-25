caaa55dfb1bff3b0901aaf693b9f424ef554da8f
James Dennes <jdennes@gmail.com>
2015-02-25 21:58:30 +0100

    Add simple spec for unauthed / request

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -7,10 +7,19 @@ describe "The Campaign Monitor Subscribe Form app" do
   let(:client_id) { ENV["GITHUB_CLIENT_ID"] }
   let(:client_secret) { ENV["GITHUB_CLIENT_SECRET"] }
 
+  describe "GET /" do
+    context "when the app is not authorised" do
+      it "redirects to request authorisation" do
+        get "/"
+        expect(last_response.status).to eq(302)
+        expect(last_response.location).to eq("http://example.org/auth/facebook")
+      end
+    end
+  end
+
   describe "GET /privacy" do
     it "shows the privacy page" do
       get "/privacy"
-
       expect(last_response.status).to eq(200)
       expect(last_response.body).to \
         include("The Campaign Monitor Subscribe Form app respect's the privacy of people who use it")
